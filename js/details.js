895eb4abc7668c73967f40118bb2538c92961ca2
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-04-18 13:19:19 +0200

    created details page and style for details page

---
+++ js/details.js
@@ -0,0 +1,31 @@
+const postContainer = document.querySelector(".details-container");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+const url = "http://cms-course-assignment.local/wp-json/wp/v2/posts/" + id;
+
+async function fetchPost() {
+  try {
+    const detailsResponse = await fetch(url);
+    const detailsResult = await detailsResponse.json();
+
+    newHtml(detailsResult);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+fetchPost();
+
+function newHtml(detailsResult) {
+  postContainer.innerHTML = `<div class="parent-container">
+                             <div class="post-text">
+                             <h2>${detailsResult.title.rendered}</h2>
+                             <p>${detailsResult.content.rendered}</p>
+                             </div>
+                             </div>`;
+};
--- js/posts.js
+++ js/posts.js
@@ -5,23 +5,33 @@ const url = "http://cms-course-assignment.local/wp-json/wp/v2/posts";
 async function fetchPosts() {
   try {
 
+    for (let i = 0; i < 100; i++) {
+      resultsContainer.innerHTML += `<div class="skeleton loader skeleton-card">
+                                     <div class="skeleton-loader skeleton-image" src=""></div>
+                                     </div>`;
+    }
+
     const response = await fetch(url);
   
     const jsonResult = await response.json();
   
-    console.log(jsonResult);
-  
-    resultsContainer.innerHTML = "";
+    resultsContainer.innerHTML = ``;
   
     const posts = jsonResult;
 
+    console.log(jsonResult);
+
     posts.forEach(function(post){
-      resultsContainer.innerHTML += `<a>
-                                     <div class="post-text">
-                                     <h2>${post.title}</h2>
-                                     <h2>${post.content}</h2>
+      resultsContainer.innerHTML += `
+                                     <div class="parent-container">
+                                     <div class="card-container">
+                                     <div class="post-title">
+                                     <h2>${post.title.rendered}</h2>
+                                     <p>${post.excerpt.rendered}</p>
+                                     <a href="/app/details.html?id=${post.id}"><button>Read more</button></a>
                                      </div>
-                                     </a>`;
+                                     </div>
+                                     </div>`;
     });
     console.log(resultsContainer);
   } catch (e){
